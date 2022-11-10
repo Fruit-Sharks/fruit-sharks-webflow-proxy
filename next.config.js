a44cd13b5c5ccae69f1a9215505397b2c2842953
James Hooper <hooper.j.1991@gmail.com>
2022-11-10 11:44:43 +0000

    feat: redirect and rewrite proxy rules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/de",
+        permanent: false,
+      },
+    ];
+  },
+
+  rewrites: async () => {
+    return [
+      {
+        source: "/de",
+        destination: "https://altes-neuwirtshaus.webflow.io",
+      },
+      {
+        source: "/de/:slug*",
+        destination: "https://altes-neuwirtshaus.webflow.io/:slug*",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
